87e0c0a3d36b6e00fa79743fb1fe2df802f6c05f
Jesse Lauesen <69030071+techFries52@users.noreply.github.com>
2021-05-07 19:23:51 -0500

    c

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Picture } from "react-responsive-picture";
+import NameLogo from "../assets/orangelogo.svg"
+import NameLogo3 from "../assets/namescifi3.png";
+import NameLogoBig from "../assets/namescifibig.png";
+import NameLogoSmol from "../assets/namescifismol.png";
 
 const AboutMe = () => {
   return (
-    <div className="row justify-content-center aboutme">
-      <div className="col-md-6">
-        <h1 className="">I'm Jesse Lauesen</h1>
-        <h2>I create stuff for the internet</h2>
-        <p className="">
-          I am a creator based in Chicago, IL that builds websites and designs
-          graphics.
-        </p>
+    <div>
+      <div className="row">
+        <img className="herologo" src={NameLogoBig} srcset={`${NameLogoSmol} 300w, ${NameLogo3} 768w, ${NameLogoBig} 1280w`} />
+      </div>
+
+      <div className="row justify-content-center aboutme">
+
+        <div className="col-md-6">
+          <h2>I create stuff for the internet</h2>
+          <p className="">
+            I am a creator based in Chicago, IL that builds websites and designs
+            graphics.
+          </p>
+        </div>
       </div>
     </div>
   );
