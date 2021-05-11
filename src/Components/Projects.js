8c36b1565bda70e2e632aa36cb71171038b3c6b8
Jesse Lauesen <69030071+techFries52@users.noreply.github.com>
2021-05-11 14:05:37 -0500

    c

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import workoutbg from "../assets/workoutbg.png"
 
 const Projects = (props) => {
   const { projects } = props;
@@ -6,13 +7,12 @@ const Projects = (props) => {
     <div className="projects">
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <div className="card">
-            <div className="card-img-top">
-
-            </div>
-            <div className="card-body">
-              <h4>Home Workout App</h4>
-              <p>An app to build your own daily workout routine</p>
+          <div class="card">
+            <img src={workoutbg} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
